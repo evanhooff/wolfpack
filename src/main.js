d200ddcddef392c8eed30226d367ed11780793b2
elinevanhooff <elinevanhooff@gmail.com>
2020-01-31 13:35:20 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const getBaseUrl = () => {
+  return "https://join.wolfpackit.nl"
+}
+
+axios.defaults.headers.common['Authorization'] = 'Bearer 9bAqXRPplyiGfF6n81NVUGpAqeLI1QHw46aqICVL1BLaGI6';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Credentials'] = true;
+
+const axiosInstance = axios.create({
+  baseURL: getBaseUrl(),
+  timeout: 5000
+});
+
+axiosInstance.get('').catch(err => {
+  console.log(err.request._header)
+})
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
