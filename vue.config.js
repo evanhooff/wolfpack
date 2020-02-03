041dab1222189ca2640a27c52a10963848366660
elinevanhooff <elinevanhooff@gmail.com>
2020-02-03 13:39:40 +0100

    set vue mode

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+    mode: process.env.NODE_ENV,
     chainWebpack: config => {
         const apiClient = process.env.VUE_APP_API_CLIENT
         config.resolve.alias.set(
