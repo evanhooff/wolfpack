78b5a28e035e544721369351461f39bc2002ceac
elinevanhooff <elinevanhooff@gmail.com>
2020-02-03 13:51:13 +0100

    refactor environment

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ Vue.config.productionTip = false;
 const getBaseUrl = () => {
   return process.env.VUE_APP_BASEURL;
 }
+console.log(getBaseUrl())
 
 axios.defaults.headers.common['Authorization'] = 'Bearer 9bAqXRPplyiGfF6n81NVUGpAqeLI1QHw46aqICVL1BLaGI6';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
@@ -18,7 +19,9 @@ const axiosInstance = axios.create({
   timeout: 5000
 });
 
-axiosInstance.get('');
+axiosInstance.get('').catch(err => {
+  console.log(err.request._header)
+})
 
 new Vue({
   render: h => h(App)
