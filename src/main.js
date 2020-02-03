f3714db9c3fa9e685340ccb4f9e4ed76a475fa18
elinevanhooff <elinevanhooff@gmail.com>
2020-02-03 14:03:42 +0100

    refactor rest interface

--- src/main.js
+++ src/main.js
@@ -1,27 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
-import axios from 'axios';
+import rest from './api/rest';
 
 Vue.config.productionTip = false;
 
-const getBaseUrl = () => {
-  return process.env.VUE_APP_BASEURL;
-}
-console.log(getBaseUrl())
-
-axios.defaults.headers.common['Authorization'] = 'Bearer 9bAqXRPplyiGfF6n81NVUGpAqeLI1QHw46aqICVL1BLaGI6';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
-axios.defaults.headers.common['Access-Control-Allow-Credentials'] = true;
-
-const axiosInstance = axios.create({
-  baseURL: getBaseUrl(),
-  timeout: 5000
-});
-
-axiosInstance.get('').catch(err => {
-  console.log(err.request._header)
-})
+rest.getBase();
 
 new Vue({
   render: h => h(App)
